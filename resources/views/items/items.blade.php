5786e192c2b9da419280c479466f7fb77b413478
nyanchan <mktds13@outlook.jp>
2018-09-15 07:45:39 +0000

    request_path

--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -21,10 +21,15 @@
                             </div>
                         </div>
                         @if (isset($item->count))
-                            <div class="panel-footer">
-                                <p class="text-center">{{ $key+1 }}位: {{ $item->count}} Wants</p>
-                                <p class="text-center">{{ $key+1 }}位: {{ $item->count}} Haves</p>
-                            </div>
+                            @if (Request::path() === 'ranking/want') 
+                                <div class="panel-footer">
+                                    <p class="text-center">{{ $key+1 }}位: {{ $item->count}} Wants</p>
+                                </div>
+                            @elseif (Request::path() === 'ranking/have')
+                                <div class="panel-footer">
+                            　　    <p class="text-center">{{ $key+1 }}位: {{ $item->count}} Haves</p>
+                                </div>
+                            @endif
                         @endif
                     </div>
                 </div>
